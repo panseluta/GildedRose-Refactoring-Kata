ac44eeded9b265d6315c9f9e3d9f908c80620dcf
Oana Corb <oana.corb@tss-yonder.com>
2020-08-28 15:02:42 +0200

    - added GildedRoseProtocol - added GildedRoseInitial implementing GildedRoseProtocol with initial implementation of updateQuality method - fixed initial updateQuality method for conjured items (fixed the case where quality was dropping below 0) - added tests for GildedRoseInitial's implementation of updateQuality

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -1,7 +1,12 @@
 import Foundation
 
-public class GildedRose {
-    var items:[Item]
+public protocol GildedRoseProtocol {
+	var items:[Item] {get}
+	func updateQuality()
+}
+
+public class GildedRose : GildedRoseProtocol {
+    public var items:[Item]
     
     public init(items:[Item]) {
         self.items = items
@@ -57,6 +62,79 @@ public class GildedRose {
 			item.quality = max(min(quality + deltaQuality, 50), 0)
 		}
 	}
+	
+}
+
+public class GildedRoseInitial : GildedRoseProtocol {
+    public var items:[Item]
+    
+    public init(items:[Item]) {
+        self.items = items
+    }
+
+	public func updateQuality() {
+        for i in 0..<items.count {
+            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i].quality > 0) {
+                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                        items[i].quality = items[i].quality - 1
+						
+						if (items[i].quality > 0) {
+							if (items[i].name.contains("Conjured")) {
+								items[i].quality = items[i].quality - 1
+							}
+						}
+                    }
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1
+                    
+                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1
+                            }
+                        }
+                        
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1
+                            }
+                        }
+                    }
+                }
+            }
+            
+            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                items[i].sellIn = items[i].sellIn - 1
+            }
+            
+            if (items[i].sellIn < 0) {
+                if (items[i].name != "Aged Brie") {
+                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if (items[i].quality > 0) {
+                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                                items[i].quality = items[i].quality - 1
+								
+								if (items[i].quality > 0) {
+									if (items[i].name.contains("Conjured")) {
+										items[i].quality = items[i].quality - 1
+									}
+								}
+                            }
+                        }
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality
+                    }
+                } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1
+                    }
+                }
+            }
+        }
+    }
 
 }
 
--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -53,6 +53,57 @@ class GildedRoseTests: XCTestCase {
 		XCTAssertEqual(0, item.quality)
 	}
 
+	
+	func setUp_initial(with name: String, sellIn: Int, quality: Int) -> Item {
+		let items = [Item(name: name, sellIn: sellIn, quality: quality)]
+		let app = GildedRoseInitial(items: items);
+		app.updateQuality();
+		
+		return app.items[0]
+	}
+	
+    func testSulfuras_initial() {
+		let item = setUp_initial(with: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80)
+		XCTAssertEqual(0, item.sellIn)
+		XCTAssertEqual(80, item.quality)
+	}
+	
+	func testConjuredItem1_initial() {
+		let item = setUp_initial(with: "Conjured Mana Cake", sellIn: 3, quality: 6)
+		XCTAssertEqual(2, item.sellIn)
+		XCTAssertEqual(4, item.quality)
+	}
+	
+	func testConjuredItem2_initial() {
+		let item = setUp_initial(with: "Conjured Mana Cake", sellIn: 3, quality: 1)
+		XCTAssertEqual(2, item.sellIn)
+		XCTAssertEqual(0, item.quality)
+	}
+	
+	func testConjuredItem3_initial() {
+		let item = setUp_initial(with: "Conjured Mana Cake", sellIn: 0, quality: 6)
+		XCTAssertEqual(-1, item.sellIn)
+		XCTAssertEqual(2, item.quality)
+	}
+	
+	func testConjuredItem4_initial() {
+		let item = setUp_initial(with: "Conjured Mana Cake", sellIn: -3, quality: 1)
+		XCTAssertEqual(-4, item.sellIn)
+		XCTAssertEqual(0, item.quality)
+	}
+	
+	func testConjuredItem5_initial() {
+		let item = setUp_initial(with: "Conjured Mana Cake", sellIn: -3, quality: 0)
+		XCTAssertEqual(-4, item.sellIn)
+		XCTAssertEqual(0, item.quality)
+	}
+	
+	func testConjuredItem6_initial() {
+		let item = setUp_initial(with: "Conjured Mana Cake", sellIn: 3, quality: 0)
+		XCTAssertEqual(2, item.sellIn)
+		XCTAssertEqual(0, item.quality)
+	}
+
     static var allTests : [(String, (GildedRoseTests) -> () throws -> Void)] {
         return [
             ("testSulfuras", testSulfuras),
@@ -61,7 +112,14 @@ class GildedRoseTests: XCTestCase {
 			("testConjuredItem3", testConjuredItem3),
 			("testConjuredItem4", testConjuredItem4),
 			("testConjuredItem3", testConjuredItem5),
-			("testConjuredItem3", testConjuredItem6)
+			("testConjuredItem3", testConjuredItem6),
+            ("testSulfuras_initial", testSulfuras_initial),
+			("testConjuredItem1_initial", testConjuredItem1_initial),
+			("testConjuredItem2_initial", testConjuredItem2_initial),
+			("testConjuredItem3_initial", testConjuredItem3_initial),
+			("testConjuredItem4_initial", testConjuredItem4_initial),
+			("testConjuredItem3_initial", testConjuredItem5_initial),
+			("testConjuredItem3_initial", testConjuredItem6_initial)
         ]
     }
 }
